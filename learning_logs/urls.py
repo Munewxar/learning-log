587ae109a3b025876d5e22e916dc9a1d105661e9
Munewxar <steven.altamirano.work@gmail.com>
2023-04-29 00:32:16 +0300

    new topic creation added

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path("topics/", views.topics, name="topics"),
     # Concrete Topic Page
     path("topics/<int:topic_id>/", views.topic, name="topic"),
+    # New Topic Page
+    path("new_topic/", views.new_topic, name="new_topic"),
 ]
