b4df4a5fddf37a77f03ed2f470902445eb10c954
Munewxar <steven.altamirano.work@gmail.com>
2023-05-03 17:07:02 +0300

    set up heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -128,5 +128,10 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
+# My Settings
 LOGIN_URL = "/users/login/"
+
+# Heroku Settings
+import django_heroku
+
+django_heroku.settings(locals())
